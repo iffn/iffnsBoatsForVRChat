5de78e6d54cac55bcf981df2bcfeda253518b9a0
iffn <iffn1990@gmail.com>
2023-08-09 23:36:37 +0200

    Implemented working collider system
    Stationary when stopped, active when driving

--- BoatController.cs
+++ BoatController.cs
@@ -14,7 +14,10 @@ public class BoatController : UdonSharpBehaviour
     [SerializeField] Rigidbody linkedRigidbody;
     [SerializeField] Transform thruster;
     [SerializeField] Transform modelHolder;
-    
+
+    [SerializeField] Collider[] stationaryColliders;
+    [SerializeField] Collider[] movingColliders;
+
     public HullCalculator LinkedHullCalculator
     {
         get
@@ -25,12 +28,70 @@ public class BoatController : UdonSharpBehaviour
 
     bool active = false;
 
+    float originalLinearDrag = 0;
+    float originalAngularDrag = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log($"Vertices count = {baseMesh.mesh.vertexCount}");
 
         linkedHullCalculator.Setup(baseMesh, baseMesh.transform, linkedRigidbody);
+
+        originalLinearDrag = linkedRigidbody.drag;
+        originalAngularDrag = linkedRigidbody.angularDrag;
+
+        StationaryMode = true;
+    }
+
+    public void StopRigidbody()
+    {
+        linkedRigidbody.velocity = Vector3.zero;
+        linkedRigidbody.angularVelocity = Vector3.zero;
+
+        modelHolder.SetPositionAndRotation(transform.position, transform.rotation);
+    }
+
+    bool StationaryMode
+    {
+        set
+        {
+            if (value)
+            {
+                modelHolder.SetPositionAndRotation(transform.position, transform.rotation);
+                modelHolder.parent = transform.parent;
+
+                linkedRigidbody.velocity = Vector3.zero;
+                linkedRigidbody.angularVelocity = Vector3.zero;
+
+                linkedRigidbody.drag = 1000000;
+                linkedRigidbody.angularDrag = 1000000;
+                SendCustomEventDelayedFrames(nameof(StopRigidbody), 1, VRC.Udon.Common.Enums.EventTiming.LateUpdate);
+                SendCustomEventDelayedFrames(nameof(StopRigidbody), 2, VRC.Udon.Common.Enums.EventTiming.LateUpdate);
+            }
+            else
+            {
+                modelHolder.parent = transform;
+
+                linkedRigidbody.drag = originalLinearDrag;
+                linkedRigidbody.angularDrag = originalAngularDrag;
+            }
+
+            active = !value;
+
+            linkedHullCalculator.disablePhysics = value;
+            linkedRigidbody.useGravity = !value;
+
+            foreach (Collider collider in stationaryColliders)
+            {
+                collider.enabled = value;
+            }
+
+            foreach (Collider collider in movingColliders)
+            {
+                collider.enabled = !value;
+            }
+        }
     }
 
     public float calculationTimeMs = 0;
@@ -39,8 +100,6 @@ public class BoatController : UdonSharpBehaviour
     // Update is called once per frame
     void Update()
     {
-        modelHolder.SetPositionAndRotation(transform.position, transform.rotation);
-
         if (active)
         {
             float target = -steeringInput * maxDeflectionAngle;
@@ -106,20 +165,16 @@ public class BoatController : UdonSharpBehaviour
     public void LocalPlayerEntered()
     {
         if (!Networking.IsOwner(gameObject)) Networking.SetOwner(Networking.LocalPlayer, gameObject);
-        
-        active = true;
 
-        linkedHullCalculator.disablePhysics = false;
-        linkedRigidbody.useGravity = true;
+        StationaryMode = false;
     }
 
     public void LocalPlayerExited()
     {
         active = false;
 
-        linkedHullCalculator.disablePhysics = true;
-        linkedRigidbody.useGravity = false;
-        linkedRigidbody.velocity = Vector3.zero;
-        linkedRigidbody.angularVelocity = Vector3.zero;
+        
+
+        StationaryMode = true;
     }
 }
