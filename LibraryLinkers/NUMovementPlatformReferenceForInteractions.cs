e340a911f2db1c2407f644607d9adb41187264e3
iffn <iffn1990@gmail.com>
2024-04-20 14:53:26 +0200

    Created boat world elements and linker script

---
+++ LibraryLinkers/NUMovementPlatformReferenceForInteractions.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using NUMovementPlatformSyncMod;
+using iffnsStuff.iffnsVRCStuff.InteractionController;
+
+public class NUMovementPlatformReferenceForInteractions : UdonSharpBehaviour
+{
+    [SerializeField] NUMovementSyncMod linkedNUMovementPlaftormMod;
+    [SerializeField] InteractionController linkedInteractionController;
+
+    public void Start()
+    {
+        if (nameof(NUMovementPlafrormChanged).Equals(linkedNUMovementPlaftormMod.PlatformChangeEventName))
+        {
+            Debug.LogWarning($"Warning: {nameof(NUMovementPlatformReferenceForInteractions)} cannot link events due to naming mismatch");
+        }
+    }
+
+    public void NUMovementPlafrormChanged()
+    {
+        linkedInteractionController.ReferenceTransform = linkedNUMovementPlaftormMod.GroundTransformLink;
+    }
+}
